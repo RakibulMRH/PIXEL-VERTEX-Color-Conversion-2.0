54d57445ff22338a087c98144e55d2fab83a316c
Rakibul Hasan <www.rakibulmrh@gmail.com>
2023-10-07 22:03:33 +0600

    Create notASource.cpp

---
+++ notASource.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+#include <stdlib.h>
+#include <string>
+
+struct Color
+{
+	int r;
+	int g;
+	int b;
+};
+/*
+Email at rakibulmrh4bd@gmail.com for the rest of the codes
+*/
